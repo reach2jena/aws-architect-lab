edb6cece442fe95fe174cf5cb42fed4f1c56444c
Rakesh419 <raech2jena@gmail.com>
2023-02-24 18:05:10 +0530

    ppm.xml changes

--- aws-ecs-testing/src/test/java/com/saanvisoft/aws/demo/AwsEcsTestingApplicationTests.java
+++ aws-ecs-testing/src/test/java/com/saanvisoft/aws/demo/AwsEcsTestingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.saanvisoft.aws.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AwsEcsTestingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.saanvisoft.aws.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AwsEcsTestingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
